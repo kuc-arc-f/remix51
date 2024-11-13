b38cf6b4540a8f779e755c5321b9e7604d2928cc
中島浩二 <knaka0209@gmail.com>
2024-11-13 10:22:46 +0900

    up, 1113a6

--- chat3/app/components/Head.tsx
+++ chat3/app/components/Head.tsx
@@ -1,21 +1,23 @@
 function Page() {
     return (
     <div>
-        <a href="/">[ Home ]</a>
-        <a href="/todo1" className="ms-2"> [ todo1 ]</a>
-        <a href="/todo4" className="ms-2"> [ todo4 ]</a>
-        <a href="/form4" className="ms-2"> [ Form4 ]</a>
-        <a href="/form5" className="ms-2"> [ Form5 ]</a>
-        <a href="/form6" className="ms-2"> [ Form6 ]</a>
-        <a href="/form10" className="ms-2"> [ Form10 ]</a>
-        <a href="/form11" className="ms-2"> [ Form11 ]</a>
-        <a href="/form12" className="ms-2"> [ Form12 ]</a> 
-        <a href="/plan1" className="ms-2"> [ Plan1 ]</a> 
-        <a href="/plan2" className="ms-2"> [ Plan2 ]</a>
-        <a href="/chat2" className="ms-2"> [ Chat2 ]</a>
-        <a href="/chat3" className="ms-2"> [ Chat3 ]</a>
-        <br />
+        <a href="/" className="font-bold ms-4" > Home </a>
+        <a href="/about" className="ms-2 text-gray-400"> [ about ]</a>
     </div>
     );
 }
 export default Page;
+/*
+<a href="/todo1" className="ms-2"> [ todo1 ]</a>
+<a href="/todo4" className="ms-2"> [ todo4 ]</a>
+<a href="/form4" className="ms-2"> [ Form4 ]</a>
+<a href="/form5" className="ms-2"> [ Form5 ]</a>
+<a href="/form6" className="ms-2"> [ Form6 ]</a>
+<a href="/form10" className="ms-2"> [ Form10 ]</a>
+<a href="/form11" className="ms-2"> [ Form11 ]</a>
+<a href="/form12" className="ms-2"> [ Form12 ]</a> 
+<a href="/plan1" className="ms-2"> [ Plan1 ]</a> 
+<a href="/plan2" className="ms-2"> [ Plan2 ]</a>
+<a href="/chat2" className="ms-2"> [ Chat2 ]</a>
+<a href="/chat3" className="ms-2"> [ Chat3 ]</a>
+*/
--- chat3/app/routes/_index.tsx
+++ chat3/app/routes/_index.tsx
@@ -10,22 +10,66 @@ export const loader: LoaderFunction = async ({ request }) => {
   await requireUserSession(request);
   return null;
 };
+//{title: "about", path: "/about"},
+const items = [
+  {title: "todo1", path: "/todo1"},
+  {title: "todo4", path: "/todo4"},
+  {title: "form4", path: "/form4"},
+  {title: "form5", path: "/form5"},
+  {title: "form6", path: "/form6"},
+  {title: "form10", path: "/form10"},
+  {title: "form11", path: "/form11"},
+  {title: "form12", path: "/form12"},
+  {title: "plan1", path: "/plan1"},
+  {title: "plan2", path: "/plan2"},
+  {title: "chat2", path: "/chat2"},
+  {title: "chat3", path: "/chat3"},
+];
 //
 const TodoApp = () => {
 
   return (
   <>
     <Head />
-    <div className="max-w-2xl mx-auto p-4">
-      <h1 className="text-2xl font-bold">hello</h1>
+    <div className="max-w-2xl mx-auto px-4 pt-2 pb-24 ">
+      <div className="flex flex-row">
+        <div className="flex-1 p-2 m-1">
+          <h1 className="text-2xl font-bold">home</h1>
+        </div>
+        <div className="flex-1 p-2 m-1 text-end">
+          <Form method="post" action="/logout">
+            <button type="submit">[ Logout ]</button>
+          </Form>
+        </div>
+      </div>
       <hr className="my-2" />
-      <Form method="post" action="/logout">
-        <button type="submit">[ Logout ]</button>
-      </Form>
+      <div className="space-y-2">
+        {items.map((todo, index) => (
+          <div key={index} 
+          className="border px-4 py-2 rounded-lg shadow hover:shadow-md transition-shadow duration-200">
+            <div className="flex justify-between items-start">
+              <div>
+                <a href={`${todo.path}`}>
+                  <h3 className="font-bold">{todo.title}</h3>
+                </a>
+              </div>
+              <div className="flex gap-2">
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   </>
   );
 };
-
 export default TodoApp;
-
+/*
+<div className="max-w-2xl mx-auto p-4">
+  <h1 className="text-2xl font-bold">hello</h1>
+  <hr className="my-2" />
+  <Form method="post" action="/logout">
+    <button type="submit">[ Logout ]</button>
+  </Form>
+</div>
+*/
